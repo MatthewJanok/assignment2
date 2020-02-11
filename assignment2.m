dc43d7120d3e6fe346f56a5118b8a87b3c7aef4b
Matthew Janok <mattjanok@cmail.carleton.ca>
2020-02-11 13:59:48 -0500

    Saddle Graph

--- assignment2.m
+++ assignment2.m
@@ -32,39 +32,39 @@ for j = 1:ny
         elseif j == ny
             G(n,:) = 0;
             G(n,n) = 1;
-            B(n,1) = 0;
+            B(n,1) = 1;
             
-%         elseif i == 1
-%             %Mapping
-%             nym = (i)+(j-2)*nx;
-%             nyp = (i)+(j)*nx;
-%             nxp = (i+1)+(j-1)*nx;
-%             G(n,n) = 1;
-%             B(n,1) = 1;
-% %             rym = (cMap(i,j) + cMap(i,j-1))/2;
-% %             ryp = (cMap(i.j) + cMap(i,j+1))/2;            
-% %             rxp = (cMap(i,j) + cMap(i+1,j))/2;
-% % 
-% %             G(n,n) = -(rxp+rym+ryp);
-% %             G(n,nym) = rym;
-% %             G(n,nyp) = ryp;
-% %             G(n,nxp) = rxp;
-%             
-%         elseif i == nx
-%             %Mapping
-%             nym = (i)+(j-2)*nx;
-%             nyp = (i)+(j)*nx;
-%             nxm = (i-1)+(j-1)*nx;
-%             G(n,n) = 1;
-%             B(n,1) = 0;
-% %             rym = (cMap(i,j) + cMap(i,j-1))/2;
-% %             ryp = (cMap(i.j) + cMap(i,j+1))/2;            
-% %             rxm = (cMap(i,j) + cMap(i-1,j))/2;
-% % 
-% %             G(n,n) = -(rxm+rym+ryp);
-% %             G(n,nym) = rym;
-% %             G(n,nyp) = ryp;
-% %             G(n,nxp) = rxp;            
+        elseif i == 1
+            %Mapping
+            nym = (i)+(j-2)*nx;
+            nyp = (i)+(j)*nx;
+            nxp = (i+1)+(j-1)*nx;
+            G(n,n) = 1;
+            B(n,1) = 0;
+%             rym = (cMap(i,j) + cMap(i,j-1))/2;
+%             ryp = (cMap(i.j) + cMap(i,j+1))/2;            
+%             rxp = (cMap(i,j) + cMap(i+1,j))/2;
+% 
+%             G(n,n) = -(rxp+rym+ryp);
+%             G(n,nym) = rym;
+%             G(n,nyp) = ryp;
+%             G(n,nxp) = rxp;
+            
+        elseif i == nx
+            %Mapping
+            nym = (i)+(j-2)*nx;
+            nyp = (i)+(j)*nx;
+            nxm = (i-1)+(j-1)*nx;
+            G(n,n) = 1;
+            B(n,1) = 0;
+%             rym = (cMap(i,j) + cMap(i,j-1))/2;
+%             ryp = (cMap(i.j) + cMap(i,j+1))/2;            
+%             rxm = (cMap(i,j) + cMap(i-1,j))/2;
+% 
+%             G(n,n) = -(rxm+rym+ryp);
+%             G(n,nym) = rym;
+%             G(n,nyp) = ryp;
+%             G(n,nxp) = rxp;            
 
         else
             %Mapping
